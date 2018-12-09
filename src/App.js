98819c79db9a8b08b72b3fe0a71481d69d4222ff
stefan judis <stefanjudis@gmail.com>
2018-12-09 15:26:04 +0100

    Initial implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { createClient } from 'contentful';
+import ReactMarkdown from 'react-markdown';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      page: null,
+      message: null
+    };
+
+    createClient({
+      space: 'y8alybrh6clk',
+      accessToken:
+        'e0257cc25552f4c398b01e411feb2790e211edd8c02845dcf00b1618ce247231'
+    })
+      .getEntries({
+        'sys.id': '6MZC43ULAWQyYGmOu4Ea64'
+      })
+      .then(({ items }) => {
+        this.setState({
+          loading: false,
+          page: items[0]
+        });
+      });
+  }
+
+  async handleContactSubmit(event) {
+    try {
+      event.preventDefault();
+      const target = event.target;
+      const response = await fetch(target.action, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8'
+        },
+        body: JSON.stringify({
+          name: target.name.value,
+          email: target.email.value,
+          receiveUpdates: target.receiveUpdates.checked,
+          message: target.message.value
+        })
+      });
+
+      if (response.status === 201) {
+        this.setState({
+          message: 'Thank you!'
+        });
+      } else {
+        throw new Error('Something went wront');
+      }
+    } catch (e) {
+      this.setState({
+        message: e.message
+      });
+    }
+  }
+
   render() {
+    const { loading, page, message } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>{page.fields.title}</h1>
         </header>
+        <main>
+          <ReactMarkdown source={page.fields.body} />
+        </main>
+        <h2>Contact</h2>
+        {message ? <p role="alert">{message}</p> : ''}
+        <form
+          className="contactForm"
+          action="/.netlify/functions/contact"
+          method="post"
+          onSubmit={this.handleContactSubmit.bind(this)}
+        >
+          <label>
+            <span>Name</span>
+            <input type="text" name="name" required />
+          </label>
+
+          <label>
+            <span>Email</span>
+            <input type="email" name="email" required />
+          </label>
+
+          <label>
+            <span>I want to receive monthly updates</span>
+            <input type="checkbox" name="receiveUpdates" />
+          </label>
+
+          <label>
+            <span>Message</span>
+            <textarea name="message" cols="30" rows="10" />
+          </label>
+
+          <button type="submit">Send message</button>
+        </form>
       </div>
     );
   }
---
+++ src/lambda/contact.js
@@ -0,0 +1,66 @@
+const Mailgun = require('mailgun-js');
+const Airtable = require('airtable');
+
+const sendMail = ({ email }) => {
+  return new Promise((resolve, reject) => {
+    const { MG_API_KEY, MG_DOMAIN } = process.env;
+    const mailgun = Mailgun({
+      apiKey: MG_API_KEY,
+      domain: MG_DOMAIN
+    });
+
+    const mailData = {
+      from: 'Stefan Judis <noreply@stefanjudis.com>',
+      to: email,
+      subject: 'Thanks for getting in touch!',
+      text: "I'll come back to you asap!"
+    };
+    mailgun.messages().send(mailData, function(e) {
+      if (e) {
+        return reject(e);
+      }
+
+      resolve();
+    });
+  });
+};
+
+const savePerson = ({ name, email, message }) => {
+  return new Promise((resolve, reject) => {
+    const { AT_API_KEY, AT_BASE, AT_TABLE } = process.env;
+    Airtable.configure({
+      endpointUrl: 'https://api.airtable.com',
+      apiKey: AT_API_KEY
+    });
+
+    const base = Airtable.base(AT_BASE);
+
+    base(AT_TABLE).create({ name: name, email: email, message: message }, e => {
+      if (e) {
+        return reject(e);
+      }
+      resolve();
+    });
+  });
+};
+
+exports.handler = async event => {
+  try {
+    const data = JSON.parse(event.body);
+    await Promise.all([
+      sendMail(data),
+      data.receiveUpdates ? savePerson(data) : null
+    ]);
+
+    return {
+      statusCode: 201,
+      body: 'Created'
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: e.message
+    };
+  }
+};
